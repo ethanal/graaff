c54fb5ef230e6d68f0dc82831a82043ac3446169
Ethan Lowman <ethan.l.527@gmail.com>
2014-10-09 23:58:14 -0400

    Add argument checks for polyline functions

--- graaff/polyline.py
+++ graaff/polyline.py
@@ -2,13 +2,23 @@
 
 # https://developers.google.com/maps/documentation/utilities/polylinealgorithm
 
+import re
+
+binary_matcher = re.compile(r"^[01]*$")
+
 
 def invert(b):
     """Invert a binary string"""
+    if binary_matcher.match(b) is None:
+        raise ValueError("expected argument 1 to be a binary string")
+
     return "".join(str(int(not int(c))) for c in b)
 
 
 def encode_float(f):
+    if abs(f) > 180:
+        raise ValueError("argument 1 must be between -180 and 180")
+
     b = bin(abs(int(round(f * 1e5))))[2:]
     b = ("0" * (32 - len(b))) + b
 
@@ -36,6 +46,9 @@ def encode_float(f):
 
 
 def encode_polyline(points):
+    # Convert any tuples to lists so they can be modified in place
+    points = map(list, points)
+
     for i in reversed(range(len(points) - 1)):
         points[i + 1][0] -= points[i][0]
         points[i + 1][1] -= points[i][1]
